a68cca6ac6898579975991612aef2e631dce1559
chrisPicano <cpicano25@gmail.com>
2021-01-27 22:32:47 -0500

    Added effective localStorage

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,35 @@
+//Manipulating DOM to show current day
 var currentDay = document.getElementById("currentDay");
 console.log(currentDay);
 currentDay.textContent=moment().format('dddd, MMMM Do');
-//Manipulating DOM
+
+//Local storage
+var save = document.getElementById("fas fa-save")
+var value =  document.getElementById("value");
+var key = 
+
+$(".save").on("click", function() {
+    console.log($(this).siblings("input").val())
+    var todo = $(this).siblings("input").val()
+    var key = $(this).parent().attr("id")
+    console.log(key)
+    localStorage.setItem(key, todo)
+})
+
+for (var i = 0; i < localStorage.length; i++) {
+    var key = localStorage.key(i);
+    var value = localStorage.getItem(key);
+    console.log("save");
+}
+
+
+function populateNotes() {
+    var keys = Object.entries(localStorage)
+    console.log(keys)
+
+    for(var item of keys) {
+        console.log(item[0], item[1]) //2D array for key&value
+        $("#" + item[0]).children('input').val(item[1])
+    }
+}
+populateNotes()
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # workDayScheduler
+On this applicated I put together a daily work scheduler for a busy employer by using jQuery. Displayed on the home page you'll find the current date and the hours for a generic work schedule. Each text back is labeled by time and the user is able to input data for the specific hour. By utulizing localStorage I was also able to make it possible for the user to save their data.
